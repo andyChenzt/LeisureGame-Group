61548a2a0f978923e01cfbc7536a93b2b5a1f372
andyChenzt <12640874@student.uts.edu.au>
2018-08-24 01:41:34 +1000

    add routes
    1. add route for score
    2. seperate socket, need to export
    3. the db logic need to add
    4. need to review together

--- server/routes/account.js
+++ server/routes/account.js
@@ -1,37 +1,52 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const User = require("../../models/User");
+const UserSession = require("../../models/UserSession");
+
+mongoose.connect("mongodb://locallhost/...");
+mongoose.Promise = global.Promise;
 
 // search id or nick name (unique)
-router.get('/account/', function(req, res, next) {
-	res.send({type: 'GET'});
-})
+router.get("/account/", function(req, res, next) {
+    res.send({ type: "GET" });
+});
 
-router.get('/account/:id', function(req, res, next) {
-	res.send({type: 'GET'});
-})
+// search id or nick name (unique)
+// input finish editing check directly
+// otherwise cannot register
+router.get("/account/:nick", function(req, res, next) {
+    res.send({ type: "GET" });
+});
 
 // sign up
-router.post('/account/signup', function(req, res, next) {
-	console.log(req.body);
-	res.send({type: 'POST'});
-})
+router.post("/account/signup", function(req, res, next) {
+    console.log(req.body);
+    //do logic to check 
 
-// sign in 
-// maybe verify at here
-router.post('/account/signin', function(req, res, next) {
-	console.log(req.body);
-	res.send({type: 'POST'});
-})
+
+    User.create(req.body).then(function(user) {
+        res.send(user);
+    }).catch(next);
+});
+
+// sign in
+// maybe verify at here, doing auth
+router.post("/account/signin", function(req, res, next) {
+    console.log(req.body);
+    res.send({ type: "POST" });
+});
 
 // change name or information
-router.put('/account/:id', function(req, res, next) {
-	res.send({type: 'PUT'});
-})
+router.put("/account/:id", function(req, res, next) {
+    // doing logic
+
+    res.send({ type: "PUT" });
+});
 
 // delete the account
-router.delete('/:id', function(req, res, next) {
-	res.send({type: 'DELETE'});
-})
-
+router.delete("/:id", function(req, res, next) {
+    res.send({ type: "DELETE" });
+});
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,42 +1,26 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
-var server = require('http').Server(app);
-var accountRoutes = require('./routes/account');
-// var io = require('socket.io')(server);
+var server = require("http").Server(app);
+var accountRoutes = require("./routes/account");
+var scoreRoutes = require("./routes/score");
+// var socketServer = 
 
 // app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
-app.use('/api', accountRoutes);
 
-// socket, need to be seperte file
-// io.sockets.on('connection', function(socket) {
-//     console.log('socket connect' + socket.id);
-//     io.emit('newMsg', "hello")
+// init route
+app.use("/api", accountRoutes);
+app.use("/api", scoreRoutes);
 
-//     socket.on('disconnect', function() {
-//         console.log('socket disconnect');
-//     })
+// error handling middleware
+// send error message and change the status
+app.use(function(err, req, res, next) {
+	res.status(422).send({error: err.message})
+});
 
-//     socket.on('data', function (data) {
-//         console.log(socket.id +': ' + data.msg);
-//         var message = {from: socket.id,
-//                         msg: data.msg}
-//         socket.emit('newMsg', message)
-//     })
+// init socket
 
-//     socket.on('mouseMove', function (data) {
-//         // console.log(socket.id + data ); //+ ': x' + data.x + ', y' + data.y
-//         socket.broadcast.emit('newMove', data);
-//     })
-
-//     socket.on('snakeMove', function(keyCode) {
-
-//         socket.broadcast.emit('moveOnOtherSide', keyCode);
-//     })
-
-// })
-
-server.listen( process.env.port || 3001, function() {
-    console.log("listening 3001")
+server.listen(process.env.port || 3001, function() {
+  console.log("listening 3001");
 });
